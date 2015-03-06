691c1aff595adb0e4e54891e29f6c11b4eacdb7b
Leo Zhao <leo.yang.zhao@gmail.com>
2015-03-05 23:26:21 -0800

    protoyped a custom view and corrected the database

--- le_pr_delivery/delivery/admin.py
+++ le_pr_delivery/delivery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from delivery.models import Users, Restaurants, RestaurantComments, VotesTable
 
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(Restaurants)
+admin.site.register(RestaurantComments)
+admin.site.register(VotesTable)
+
