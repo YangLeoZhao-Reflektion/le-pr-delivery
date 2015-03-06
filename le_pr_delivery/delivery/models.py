a3f964023bd10d54b5a3d80bb360bfc2a258c9f9
Leo Zhao <leo.yang.zhao@gmail.com>
2015-03-05 20:16:46 -0800

    fixed typos in models

--- le_pr_delivery/delivery/models.py
+++ le_pr_delivery/delivery/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 class Users(models.Model):
 	Email = models.EmailField()
 	Votes_Left = models.IntegerField()
+
 
 class Restaurants(models.Model):
 	Name = models.TextField()
@@ -13,12 +15,14 @@ class Restaurants(models.Model):
 	Total_Rating = models.IntegerField()
 	Total_Count = models.IntegerField()
 
-class Restaurant_Comments(models.Model):
+
+class RestaurantComments(models.Model):
 	Uid = models.ForeignKey('Users')
 	Rid = models.ForeignKey('Restaurants')
 	Comment = models.TextField()
 
-class Votes_table(models.Model):
+
+class VotesTable(models.Model):
 	Uid = models.ForeignKey('Users')
 	Vid = models.ForeignKey('Restaurants')
 	Votes = models.TextField()
