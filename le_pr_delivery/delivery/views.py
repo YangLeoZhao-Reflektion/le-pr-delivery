a37f8235b175de01c4d19a2005bf78ffe96f5910
Leo Zhao <leo.yang.zhao@gmail.com>
2015-03-06 20:49:46 -0800

    got custom views to load

--- le_pr_delivery/delivery/views.py
+++ le_pr_delivery/delivery/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	template = loader.get_template('index.html')
+	context = RequestContext(request)
+	return HttpResponse(template.render(context))
