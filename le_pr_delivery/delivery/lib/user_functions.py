b30be26456cc31da4a160288078cef58fb5a480d
Pranjal Shah <p37shah@uwaterloo.ca>
2015-03-17 18:35:19 -0700

    adding Users model related functions

---
+++ le_pr_delivery/delivery/lib/user_functions.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from delivery.models import Users
+
+
+@login_required
+def get_users_entry(request):
+	current_user = request.user
+	try:
+		user_entry = Users.objects.get(id=current_user.id)
+	except Users.DoesNotExist:
+		user = Users(id=current_user.id, Email=current_user.email, Votes_Left=2)
+		user.save()
+		user_entry = user
+	print(user_entry.id)
