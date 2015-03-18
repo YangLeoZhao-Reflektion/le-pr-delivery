d533c357c8650453a698394f9504849e11571b83
Leo Zhao <leo.yang.zhao@gmail.com>
2015-03-17 18:56:13 -0700

    UI commit

--- le_pr_delivery/delivery/urls.py
+++ le_pr_delivery/delivery/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'le_pr_delivery.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^main_page/$', views.mainPage),
+    url(r'^restaurant/$', views.restaurant),
 	url(r'^$', views.index)
 )
--- le_pr_delivery/delivery/views.py
+++ le_pr_delivery/delivery/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
+import json
+from models import Restaurants
 
 def index(request):
 	return render_to_response('le_pr_delivery/index.html')
@@ -9,5 +11,15 @@ def index(request):
 def mainPage(request):
 	return render_to_response('le_pr_delivery/main_page.html')
 
-def bootstrap_css(request):
-	return render_to_response('le_pr_delivery/bootstrap.css')
+def restaurant(request):
+	return_list = []
+	all_restaurants = Restaurants.objects.all()
+	for restaurant in all_restaurants:
+		return_list.append(restaurant.__names__())
+		return_list.append(',')
+	#object = (data=models(Foo.objects.get(pk=object_id)))
+	return_list.append('hi')
+	print return_list
+	return HttpResponse(return_list)
+	#return render_to_response({'data': models.Restaurants.__name__})
+	# return render_to_response('le_pr_delivery/main_page.html', {'data': str(models.Restaurants.__name__)})
