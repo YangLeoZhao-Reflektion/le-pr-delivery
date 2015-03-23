2db73105fa9edae269dd083631e2175d28038b84
Pranjal Shah <p37shah@uwaterloo.ca>
2015-03-22 20:15:23 -0700

    Adding the html component for the search an ajax call is made on keyup(). backend responds with a json array of restaurants TODO- dropdown display

--- le_pr_delivery/delivery/views.py
+++ le_pr_delivery/delivery/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponseRedirect
+import json
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.api import get_messages
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
 from social_auth import __version__ as version
 from delivery.lib.model_related.restaurant_functions import search_restaurants
 
@@ -14,6 +16,7 @@ def index(request):
 	return render_to_response('le_pr_delivery/index.html')
 
 
+@csrf_exempt
 def mainPage(request):
 	return render_to_response('le_pr_delivery/main_page.html')
 
@@ -57,10 +60,24 @@ def logout(request):
 	return HttpResponseRedirect('/')
 
 
+@csrf_exempt
 def search(request):
-	query = request.GET['query']
-	if query:
-		results = search_restaurants(query)
-		for result in results:
-			print result.__names__()
-	return HttpResponseRedirect('/')
+	print "bk1"
+	if request.method == 'POST':
+		query = request.POST.get('query')
+		response_data = []
+		print query
+		if query:
+			restaurants = search_restaurants(query)
+			for restaurant in restaurants:
+				response_data.append({"id": restaurant.id, "name": restaurant.__names__()})
+
+		return HttpResponse(
+			json.dumps(response_data),
+			content_type="application/json"
+		)
+	else:
+		return HttpResponse(
+			json.dumps({"nothing to see": "this isn't happening"}),
+			content_type="application/json"
+		)
